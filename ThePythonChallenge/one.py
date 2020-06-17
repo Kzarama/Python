10ef88b5eed6b619672541759676fb86e5ca607c
kzarama <kevin.zarama1@correo.icesi.edu.co>
2020-06-17 17:57:04 -0500

    comment deleted

--- ThePythonChallenge/one.py
+++ ThePythonChallenge/one.py
@@ -1,17 +1,3 @@
-"""
-#s = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
-s= "map"
-newS = ""
-for x in range(len(s)):
-    if(ord(s[x]) > 96 and ord(s[x]) < 121):
-        newS += chr(ord(s[x]) + 2)
-    elif(ord(s[x]) > 120 and ord(s[x]) < 123):
-        newS += chr(ord(s[x]) - 24)
-    else:
-        newS += s[x]
-print(newS)
-"""
-#--------------------------------------------#
-from urllib import *
+from urllib.request import urlopen
 
-page = urlopen("http://www.pythonchallenge.com/pc/def/ocr.html");
+page = urlopen("http://www.pythonchallenge.com/pc/def/ocr.html")
