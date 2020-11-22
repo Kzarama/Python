ddd70dbb9bb1dd48f3325456f9c24d4b9279624c
kzarama <kevin.zarama1@correo.icesi.edu.co>
2020-11-21 21:55:14 -0500

    test pywhatkit

---
+++ pywhatkit_/test_pywhatkit.py
@@ -0,0 +1,16 @@
+import pywhatkit
+
+# send message of whatsapp
+# pywhatkit.sendwhatmsg('+573218807100', 'message from python', 21, 46)
+
+# play on youtube
+# pywhatkit.playonyt('ya me hiciste mal')
+
+# search on google
+# pywhatkit.search('python')
+
+# image to ascii art
+# pywhatkit.image_to_ascii_art('2.jpg')
+
+# information about a topic from wikipedia
+# pywhatkit.info('Python (programming language)', lines=3)
